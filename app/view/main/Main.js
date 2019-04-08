342bef509a2d86ed2a74bf6d1635c2a944920b3c
wtt155 <wtt155@localhost.localdomain>
2019-04-08 17:27:22 +0530

    first commit

---
+++ app/view/main/Main.js
@@ -0,0 +1,14 @@
+Ext.define('Contacts.view.main.Main',{
+    extend: 'Ext.tab.Panel',
+    items: [
+        {
+            title: 'Home'
+        },{
+            title: 'Home'
+        },{
+            title: 'Leads'
+        },{
+            title: 'Contacts'
+        }
+    ]
+});
